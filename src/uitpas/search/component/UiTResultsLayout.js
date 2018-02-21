f555bac8809b9719496c89bc284085c407aeb75d
sten <sten.govaerts@epfl.ch>
2018-02-21 10:28:52 +0100

    fixed the reload after going back to the results page from a detail page.

--- src/uitpas/search/component/UiTResultsLayout.js
+++ src/uitpas/search/component/UiTResultsLayout.js
@@ -4,8 +4,14 @@ import UiTPasSearchConfig from "../UiTPasSearchConfig";
 export default class UiTResultsLayout extends React.Component{
 
     componentWillMount() {
+        //this extra forced search is necessary to make sure when one navigates back from a detail view
+        //that the search results and filters are properly refreshed (sometimes the filters were not
+        //shown).
         if(this.props.searchkit && !UiTPasSearchConfig.globalState.refreshSearchPerformed) {
-            this.props.searchkit.searchFromUrlQuery(window.location.search);
+            //it appears that run InitialSearch is the only way to bypass the similarity checks of
+            // searchkit on the old query and the new query. when using an other function the old and
+            // new query are the same and no search is executed.
+            this.props.searchkit.runInitialSearch();
             UiTPasSearchConfig.globalState.refreshSearchPerformed = true;
             this.props.searchkit.addResultsListener(function(results){
                 UiTPasSearchConfig.globalState.refreshSearchPerformed = false;
