746be081ca998065a41ee27c91c93eb781b3b73d
brampauwelyn <brampauw@gmail.com>
2021-07-08 10:35:54 +0200

    UPS-3487 - Make seperate component for OnlineLabel

---
+++ src/uitpas/search/component/OnlineLabel.js
@@ -0,0 +1,30 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import UiTPasSearchConfig from "../UiTPasSearchConfig";
+
+const CashInType = {
+  ONLINE: "ONLINE",
+};
+
+export class OnlineLabel extends React.Component {
+  static propTypes = {
+    cashInType: PropTypes.array,
+  };
+
+  isOnlineExchangeable() {
+    const cashInType = this.props.cashInType;
+    return cashInType ? cashInType.includes(CashInType.ONLINE) : false;
+  }
+
+  render() {
+    if (
+      UiTPasSearchConfig.get("showOnlineExchangeInfo") &&
+      this.isOnlineExchangeable()
+    ) {
+      return <div className="online-exchangeable">online om te ruilen</div>;
+    }
+
+    return null;
+  }
+
+}
